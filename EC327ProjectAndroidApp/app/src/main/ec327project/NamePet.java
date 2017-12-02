a7dd6976a459b6356f26089775aa53d0f79b873d
pollybainbridge <33497245+pollybainbridge@users.noreply.github.com>
2017-12-02 12:05:36 -0500

    Add files via upload
    Added java files

---
+++ EC327ProjectAndroidApp/app/src/main/ec327project/NamePet.java
@@ -0,0 +1,13 @@
+package pbain.bu.edu.ec327project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NamePet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_pet);
+    }
+}
